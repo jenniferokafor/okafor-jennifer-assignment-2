6d4ef8347c4d3f8c8148d2be5634c68ffa22c10b
Jennifer Okafor <75744277+jenniferokafor@users.noreply.github.com>
2022-07-15 19:02:37 +0000

    adding assignment two

--- src/assignment.js
+++ src/assignment.js
@@ -33,9 +33,16 @@ assignment.sumOfNumbers = sumOfNumbers;
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    if (arrayOfNumbers[i] % 2 === 0) {
+      count ++
+    } 
+  }
+  return count
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
